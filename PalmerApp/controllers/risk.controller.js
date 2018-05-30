55bc318c188ba80e934125fe085f94f9de9f30d9
Drew Roen <drewroen@gmail.com>
2018-05-30 16:55:23 -0500

    Added the ability to round to the nearest coordinate.

--- PalmerApp/controllers/risk.controller.js
+++ PalmerApp/controllers/risk.controller.js
@@ -22,7 +22,19 @@ exports.getRisk = async function(req, res, next)
 exports.getRiskFromLocation = async function(req, res, next) 
 {
 	try{
-		var risk = await Risk.findOne({'latitude' : req.params.latitude, 'longitude' : req.params.longitude});
+		var tempLat = (parseFloat(req.params.latitude) - 44.57338516) / -0.00061283;
+		var tempLong = (parseFloat(req.params.longitude) + 97.59552151) / 0.000838633;
+		tempLat = Math.round(tempLat / 10) * 10;
+		tempLong = Math.round(tempLong / 10) * 10;
+		console.log(tempLat);
+		console.log(tempLong);
+		tempLat = 44.57338516 + (tempLat * -0.00061283)
+		tempLong = -97.59552151 + (tempLong * 0.000838633)
+		var finalLat = tempLat.toString();
+		var finalLong = tempLong.toString();
+		console.log(tempLat);
+		console.log(tempLong);
+		var risk = await Risk.findOne({'latitude' : finalLat, 'longitude' : finalLong});
 
 		return res.json(risk);
 	} catch(e){
