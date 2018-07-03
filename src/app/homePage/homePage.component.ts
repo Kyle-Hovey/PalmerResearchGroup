b8126d98c0f3dda854a3bdfda839b9f9d14cb31a
Kyle Hovey <kyle.hovey@my.simpson.edu>
2018-07-03 16:41:11 -0500

    Added blog post and twitter feed to homepage

--- src/app/homePage/homePage.component.ts
+++ src/app/homePage/homePage.component.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from "@angular/router";
+import { Post } from "../post";
+import { BlogService } from "../blog.service";
 
 @Component({
     selector: 'app-homePage',
     templateUrl: './homePage.component.html',
     styleUrls: ['./homePage.component.css']
 })
-export class HomePageComponent {
+export class HomePageComponent implements OnInit {
 
-    constructor(private router: Router) { }
+    constructor(private router: Router, private blogService: BlogService) { }
+
+	newestPost: Post;
+
+	ngOnInit() {
+        this.getNewestPost();
+    }
+
+    getNewestPost() {
+        this.blogService.getNewest()
+        .subscribe((data:Post) => {
+            if(!data) {
+            	console.log("no posts")
+            } else {
+                console.log(data);
+                this.newestPost = data;
+            }
+        });
+    }
 
 }
